152bea92341f750facdba0abc533fcf1e80a9a4c
Peter Kovacs <peter.kovacs@dpc.hu>
2019-06-26 17:06:31 +0200

    route added

--- src/main/java/com/sol/sqlreadercamel/SqlreadercamelApplication.java
+++ src/main/java/com/sol/sqlreadercamel/SqlreadercamelApplication.java
@@ -3,6 +3,8 @@ package com.sol.sqlreadercamel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class SqlreadercamelApplication {
 
